1e932b9605f14f0819bd881101a45171e601a50c
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2023-10-29 12:51:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,74 @@
-const http = require('http');
+// Create web server to handle comments
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+// Import models
+const Comments = require('../models/comments');
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Create router object
+const commentRouter = express.Router();
+
+// Use body-parser
+commentRouter.use(bodyParser.json());
+
+// Create routes
+commentRouter.route('/')
+    // Get all comments
+    .get((req, res, next) => {
+        Comments.find({})
+            .then((comments) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comments);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    // Post new comment
+    .post((req, res, next) => {
+        Comments.create(req.body)
+            .then((comment) => {
+                console.log('Comment created: ', comment);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    // Put method not supported
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /comments');
+    })
+    // Delete all comments
+    .delete((req, res, next) => {
+        Comments.deleteMany({})
+            .then((response) => {
+                console.log('All comments deleted');
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+commentRouter.route('/:commentId')
+    // Get comment with specific ID
+    .get((req, res, next) => {
+        Comments.findById(req.params.commentId)
+            .then((comment) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    // Post method not supported
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /comments/${req.params.commentId}`);
+    })
+    // Put comment with specific ID
+    .put((req,
